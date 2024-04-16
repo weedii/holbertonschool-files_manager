6cccd3e0eba2e938631791069cb0121822d80666
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2024-04-16 23:57:48 +0100

    MongoDB utils

--- utils/db.js
+++ utils/db.js
@@ -1,10 +1,10 @@
-import { MongoClient } from "mongodb";
+import { MongoClient } from 'mongodb';
 
 class DBClient {
   constructor() {
-    const host = process.env.DB_HOST || "localhost";
+    const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
-    const database = process.env.DB_DATABASE || "files_manager";
+    const database = process.env.DB_DATABASE || 'files_manager';
 
     this.client = new MongoClient(`mongodb://${host}:${port}`, {
       useUnifiedTopology: true,
@@ -13,9 +13,9 @@ class DBClient {
 
     this.client.connect((err) => {
       if (err) {
-        console.error("MongoDB Connection Error:", err);
+        console.error('MongoDB Connection Error:', err);
       } else {
-        console.log("Connected successfully to MongoDB");
+        console.log('Connected successfully to MongoDB');
       }
     });
   }
@@ -25,11 +25,11 @@ class DBClient {
   }
 
   async nbUsers() {
-    return this.client.db(this.dbName).collection("users").countDocuments();
+    return this.client.db(this.dbName).collection('users').countDocuments();
   }
 
   async nbFiles() {
-    return this.client.db(this.dbName).collection("files").countDocuments();
+    return this.client.db(this.dbName).collection('files').countDocuments();
   }
 }
 
