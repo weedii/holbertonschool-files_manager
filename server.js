9352bcaf38a9cdaef3f328eb7f98bd252174fe7e
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2024-04-28 00:40:30 +0100

    First API

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-import express from "express";
-import router from "./routes/index.js";
-import dotenv from "dotenv";
+import express from 'express';
+import router from './routes/index.js';
+import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
 
-app.use("/", router);
+app.use('/', router);
 
 const PORT = process.env.PORT || 5000;
 
