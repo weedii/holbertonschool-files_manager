56297d5d29e2195fc0ee23e98b94fedbab7f8ff3
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2024-04-27 23:09:04 +0100

    First API

--- utils/redis.js
+++ utils/redis.js
@@ -3,6 +3,7 @@ import redis from "redis";
 class RedisClient {
   constructor() {
     this.client = redis.createClient();
+    this.client.connect()
 
     this.client.on("connect", () => {
       console.log("Connected to Redis server");
