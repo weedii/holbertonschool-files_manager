006ecd2bb77662000e5603ecb60aa809f6701c34
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2024-04-28 00:14:06 +0100

    First API

--- utils/redis.js
+++ utils/redis.js
@@ -1,33 +1,53 @@
-import redis from "redis";
+import Redis from "redis";
 
 class RedisClient {
-	constructor() {
-		this.client = redis.createClient();
+  constructor() {
+    this.client = Redis.createClient();
 
-		this.getAsync = promisify(this.client.get).bind(this.client);
+    this.client.on("error", (err) => {
+      console.error("Redis client error:", err);
+    });
+  }
 
-		this.client.on('error', error => {
-			console.error(`Redis client not connected to the server: ${error}`);
-		});
-	}
+  isAlive() {
+    return this.client.connected;
+  }
 
-	isAlive() {
-		return this.client.connected;
-	}
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  }
 
-	async get(key) {
-		const value = await this.getAsync(key);
-		return value;
-	}
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, duration, value, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  }
 
-	async set(key, value, duration) {
-		this.client.set(key, value);
-		this.client.expire(key, duration);
-	}
-
-	async del(key) {
-		this.client.del(key);
-	}
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  }
 }
 
 const redisClient = new RedisClient();
